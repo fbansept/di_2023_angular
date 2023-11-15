bf9ec3962d3f3f65f7f71a48e7b1c5ac4869e71a
fbansept <bansept.franck@gmail.com>
2023-11-15 14:50:45 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'di_2023';
+  productList: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get('http://localhost:8080/products')
+      .subscribe((productList: any) => 
+          this.productList = productList);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [importProvidersFrom(HttpClientModule)],
-}).catch((err) => console.error(err));
-function importProvidersFrom(HttpClientModule: any) {
-  throw new Error('Function not implemented.');
-}
-
+});
